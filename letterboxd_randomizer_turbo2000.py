4d8a4e714b188c57e069920f507f1f76bab3536d
ivanchel <ivanmakulovdiur@gmail.com>
2026-02-28 13:05:43 +0300

    commit msg

--- letterboxd_randomizer_turbo2000.py
+++ letterboxd_randomizer_turbo2000.py
@@ -2,24 +2,30 @@ import random
 import requests
 from bs4 import BeautifulSoup
 import csv
-from time import sleep
-
-    # 1. Выбор аккаунта
+headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/121.0.0.0 Safari/537.36"
+    )
+}
+   
 username = input('Enter Letterboxd username: ')
+print('Wait a little...')
+print('_____________________')
 
-def page_reading(username, function_page):
+def page_reading(username, page_number):
     
-        # 2. Отправка GET-запроса
-    url = f'https://letterboxd.com/{username}/watchlist/page/{function_page}/'
-    response = requests.get(url)
+        # 1. Sending GET-request
+    url = f'https://letterboxd.com/{username}/watchlist/page/{page_number}/'
+    response = requests.get(url,headers=headers)
 
-        # 3. Парсинг содержимого
+        # 2. Content parsing
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-    else:
-        print("Ошибка при получении страницы")
-
-            # 4. Извлечение данных
+    else: print("Ошибка при получении страницы")
+    
+        # 3. Data extraction
     data = [tag.get('data-item-name') for tag in soup.find_all('div')]
     if data:
         function_page_films = [film for film in data if not film == None] 
@@ -27,20 +33,34 @@ def page_reading(username, function_page):
 
     return function_page_films
 
-    # 5. Извлечение фильмов со всех страниц
+    # 4. Collecting all films
 all_pages_films = []
     
 for page in range(1,100):
-    sec = [3,4,5,6,7,8,9,10]
-    sleep(random.choice(sec))
     page_films = page_reading(username, page)
-    if page_films == []: break
-    elif page_films == all_pages_films: break
-    else:
-        print(page_films)
-        all_pages_films += page_films
+        
+    if page_films == (all_pages_films or []):
+        print(all_pages_films)
+        break
+    else: all_pages_films += page_films
 
-    # 6. Определение случайного фильма
-print(random.choice(all_pages_films))
-print(all_pages_films)
-
+    # 5. Finding 'Thursday' movie because it is cool film
+if 'Thursday (1998)' in all_pages_films:
+    print('Thursday (1998)\nThere is no more choice for you')
+    
+    # 6. Choosing random film
+else:
+    random_films = [random.choice(all_pages_films) for film in range(5)]
+    choice_num = 0
+    
+    while choice_num < len(random_films):
+        print(random_films[choice_num])
+        print('_____________________')
+        film_choice = input('Show next film? - ')
+        
+        if film_choice == 'yes' or 'Yes' or 'YES':
+            choice_num += 1
+        else: break
+        
+    if choice_num == len(random_films):
+        print('Try to finally select one of these films')
