17d3de69718151487cb67708f41ed4dced92516c
ivanchel <ivanmakulovdiur@gmail.com>
2026-03-07 15:52:41 +0300

    feature: return random film from watchlist

--- main.py
+++ main.py
@@ -3,20 +3,58 @@ import os
 import requests
 from fastapi import FastAPI, Request
 
+import random
+from bs4 import BeautifulSoup
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = FastAPI()
 
-def getIsRandomizeCommand(text: str):
-    s = text.split(' ')
-    if (len(s) != 2):
-        return False, None
+headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/121.0.0.0 Safari/537.36"
+    )
+}
+   
+def page_reading(username, page_number):
+    # 1. Sending GET-request
+    url = f'https://letterboxd.com/{username}/watchlist/page/{page_number}/'
+    response = requests.get(url,headers=headers)
+    print(f'Making a request to get {username}\'s {page_number} wl page')
 
-    command, username = s
-    if (command != '/random'):
-        return False, None
+    # 2. Content parsing
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+    else: print("Ошибка при получении страницы")
+    
+    # 3. Data extraction
+    data = [tag.get('data-item-name') for tag in soup.find_all('div')]
+    if data:
+        function_page_films = [film for film in data if not film == None] 
+    else: function_page_films = []
 
-    return True, username
+    return function_page_films
+
+def getWatchlist(username: str):
+    all_pages_films = []
+    
+    for page in range(1,100):
+        page_films = page_reading(username, page)
+            
+        if page_films == []:
+            break
+        else: all_pages_films += page_films
+    
+    return all_pages_films
+
+def chooseRandomFilm(watchlist: list):
+    if 'Thursday (1998)' in watchlist:
+        return 'Thursday (1998)'
+        
+    random_film = random.choice(watchlist)
+    return random_film
 
 def send_message(chat_id, text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -30,8 +68,10 @@ def processRandomCommand(text: str, chat_id: str):
     
     _, username = s
     print('received get random film command for user ' + username)
-    send_message(chat_id, f'heyo {username}, we\'re working on this command')
+    watchlist = getWatchlist(username)
+    random_film = chooseRandomFilm(watchlist)
 
+    send_message(chat_id, random_film)
 
 # TODO: add secret prefix after webhook path
 @app.post('/webhook')
